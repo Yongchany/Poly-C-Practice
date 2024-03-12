ee35228bb114dfcb89c03bf53f652b3f5adc1df4
AISW-509-181 <AISW-509-181@DESKTOP-MCROOJ6>
2024-03-12 10:25:57 +0900

    초기 커밋.

---
+++ Project1/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+	printf("반갑습니다. 프로그램실습 시간입니다.\n");
+	printf("정말 반갑습니다.\n");
+
+	//한줄 주석문 예시
+
+	/*여러줄
+	주석문
+	예시*/
+
+	scanf_s("% s", &name, sizeof(변수명));
+	return 0;
+}
